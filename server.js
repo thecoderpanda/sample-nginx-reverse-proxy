d8c58d30a82311a62d14baeed14fc91e50f9bb60
shantanu <mailvishwanadha@gmail.com>
2020-06-02 14:23:22 +0530

    new

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const port = 3000
 
 app.get('/', (req, res) => res.send("hello world"))
 
-app.listen(port, () => console.log("Working"))
+app.listen(port, () => console.log("Duniya Madarchod hai"))
